3a9371bb1bf34820202c3979a12794ab91ae7594
t6yd6yiu6er6r <constsachinmalik@gmail.com>
2025-02-27 11:58:00 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to the Beauty Filters App</h1>
+            <p>Apply 90s aesthetic filters and save your memories!</p>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
